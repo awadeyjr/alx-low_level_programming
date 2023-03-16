bccbcfadbf60ba5679e186ac1af6a8cefb091266
mohamed elawadey <elawadey7777@icloud.com>
2023-03-16 08:32:13 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-*main - entry point
-* 
-*Return: Always 0 (Success)
-*/
+ * main	- aprogram that prints aline with puts func
+ * Return : 0 (success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
