749557791599669d5284eced915ffc154d2b1a8f
mohamed elawadey <elawadey7777@icloud.com>
2023-03-22 16:42:59 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,25 @@
 #include "main.h"
 
 /**
- * print_sign - Determines if inputs is postive or negative 
- *
+ * print_sign - Determines if inputs is postive or negative
  * @n: The input number as integer
- *
  * Return: 1 is greater than zero . 0 is zero . -1 is less than zero
  */
 int print_sign(int n)
-
 {
 	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if (n == 0)
-	{
-		_putchar(48);
-		return (0);
-	}
-	else
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
 }
