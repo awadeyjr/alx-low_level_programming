48e52d4f801f668f02d9d717e7bfc5e408aa69ba
mohamed elawadey <elawadey7777@icloud.com>
2023-03-22 12:31:42 -0700

    print alphabet 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,12 @@ void print_alphabet_x10(void)
 {
 	int i;
 	int z;
-	
-	for (z = 1 ; z <= 10; z++)
+
+	for (i = 1 ; i <= 10; ++i)
 	{
-		for (i = 97; i <= 122; ++i)
+		for (z = 97 ; z = 122; ++z)
 		{
-			_putchar(i);
+			_putchar(z);
 		}
 		_putchar('\n');
 	}
