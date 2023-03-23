2d64588bd554dda026fe94847ada1c9338f76f33
mohamed elawadey <elawadey7777@icloud.com>
2023-03-22 18:22:32 -0700

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * time_table - Prints the 9 time table
+ *
+ * example table
+ * 0, 0, 0, 0,
+ * 0, 1, 2, 3,
+ */
+void time_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(48);
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			prod = num * mult;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48);
+			_putchar((prod % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
