c4085225db6e8e64b50dc2d9ae64c8e638bb7591
mohamed elawadey <elawadey7777@icloud.com>
2023-03-23 18:39:26 -0700

    printer v2

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
- * print_most_numbers - print 0 - 9 apart
+ * print_most_numbers - print 0 - 9 apart from 2 and 4
  *
  * Return: Always 0 (Success)
  */
+
 void print_most_numbers(void)
 {
-	int i;
+	int num;
 
-	for (i = 0; i <= 9; ++i)
+	for (num = 0; num <= 9; ++num)
 	{
-		if (i == 2 || i == 4)
-			continue ;
+		if (num == 2 || num == 4)
+			continue;
 		_putchar(i + 48);
 	}
 	_putchar('\n');
