a5aac96874178a6f52363c533307a3bba3d3822c
mohamed elawadey <elawadey7777@icloud.com>
2023-03-23 19:21:01 -0700

    print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,12 +10,12 @@ void print_line(int n)
 {
 	int x;
 
-	if (n < 0)
+	if (n <= 0)
 		_putchar('\n');
 	else
 	{
 		for (x = 1; x <= n; ++x)
-			_putchar('\n');
+			_putchar('_');
 		_putchar('\n');
 	}
 }
