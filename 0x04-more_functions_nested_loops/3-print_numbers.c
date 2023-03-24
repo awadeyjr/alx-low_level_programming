ae8c665bd4dca17207dc284645d586b4895979bb
mohamed elawadey <elawadey7777@icloud.com>
2023-03-23 17:54:41 -0700

    printer

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,9 +2,12 @@
 /**
  * print_numbers - print from 0 to 9 followed by a new line
  *
+ * Return: Always 0 (Success)
  */
 void print_numbers(void)
 {
+	int i = 0;
+
 	for (i = 48; i <= 57; ++i)
 	{
 		_putchar(i);
