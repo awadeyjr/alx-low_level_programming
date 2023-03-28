8e25724bb0b6a7ff2aa0b431313f9a01cb2889ec
mohamed elawadey <elawadey7777@icloud.com>
2023-03-28 11:12:27 -0700

    first ptr

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takesa pointer to an int parameter and pdates to 98
+ *
+ * @n: int parameter
+ *
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
