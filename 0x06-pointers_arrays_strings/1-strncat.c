f5c6de3e8ab260aad4cdb733303b22192b12ab41
mohamed elawadey <elawadey7777@icloud.com>
2023-04-04 04:16:17 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,15 +8,15 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-        int destlen = 0;
-        int srclen = 0;
-        int i;
+	int destlen = 0;
+	int srclen = 0;
+	int i;
 
-        for (i = 0 ; dest[i] != '\0' ; ++i)
-                destlen++;
-        for (i = 0 ; src[i] != '\0' ; ++i)
-                srclen++;
-        for (i = 0 ; i < n ; ++i)
-                dest[destlen + i] = src[i];
-        return (dest);
+	for (i = 0 ; dest[i] != '\0' ; ++i)
+		destlen++;
+	for (i = 0 ; src[i] != '\0' ; ++i)
+		srclen++;
+	for (i = 0 ; i < n ; i++)
+		dest[destlen + i] = src[i];
+	return (dest);
 }
