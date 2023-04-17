b40e1c67a3ec0e023da3a023fd0f27c52ccd54c7
mohamed elawadey <elawadey7777@icloud.com>
2023-04-17 16:57:36 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints a number followed by number
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
