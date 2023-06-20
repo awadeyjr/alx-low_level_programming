4a350565e57bd593a0307c3675f5bbb7544092a4
awadeyjr <elawadey7777@icloud.com>
2023-06-20 09:09:30 -0700

    program to get the file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints the file name of the source file
+ * main - prints the name of the file
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	return(0);
+	return (0);
 }
