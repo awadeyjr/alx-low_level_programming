a91633db9d7cdde472e3e9f0b02c0cd765ef0b86
awadeyjr <elawadey7777@icloud.com>
2023-06-21 02:50:14 -0700

    task number 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,12 +8,15 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
-	if (array&&size&&cmp)
-		while(i < size){
-			if(cmp(array[i]))
-				return (i);
-			i++;
-		}
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
 	return (-1);
 }
