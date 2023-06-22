5d13ae09dd98d5a99e917d085d34f3d799b374bd
awadeyjr <elawadey7777@icloud.com>
2023-06-22 02:21:44 -0700

    vardiac is fun

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("(nil)");
 		}
-		printf("%s", x);
+		else
+		{
+			printf("%s", x);
+		}
 		if (separator != NULL && i < n - 1)
 		{
 			printf("%s", separator);
