81a7de63392063270871108f15cef728fa9cc2d9
awadeyjr <elawadey7777@icloud.com>
2023-06-22 02:23:03 -0700

    vardiac is fun

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,30 +2,24 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - Returns the sum of all its parameters.
- * @n: The number of parameters passed to the function.
- * @...: A variable number of parameters to calculate the sum of.
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
  *
  * Return: If n == 0 - 0.
  *         Otherwise - the sum of all parameters.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list args;
-    int sum = 0;
-    unsigned int i;
+	va_list ap;
+	unsigned int i, sum = 0;
 
-    if (n == 0)
-    {
-	    return 0;
-    }
-    va_start(args, n);
-    for (i = 0; i < n; ++i)
-    {
-	    int num = va_arg(args, int);
-	    sum += num;
-    }
+	va_start(ap, n);
 
-    va_end(args);
-    return sum;
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+
+	return (sum);
 }
